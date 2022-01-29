7f163b29a238c759220083fe8d93c3cb03031a68
mcclintsayshi <manakanejustin@gmail.com>
2022-01-29 14:11:38 +0200

    added avatar component for future updates

---
+++ components/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <Avatar
+        avatarStyle="Circle"
+        topType="ShortHairDreads02"
+        accessoriesType="Prescription02"
+        hairColor="Black"
+        facialHairType="Blank"
+        clotheType="GraphicShirt"
+        clotheColor="Heather"
+        graphicType="Diamond"
+        eyeType="Default"
+        eyebrowType="DefaultNatural"
+        mouthType="Smile"
+        skinColor="Brown"
+      />
+    </div>
+  );
+}
+
+export default About;
