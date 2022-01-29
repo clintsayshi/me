51de88c5fd803ccb61f76b61ddf37df0e11de86d
mcclintsayshi <manakanejustin@gmail.com>
2022-01-29 15:33:11 +0200

    updated google analytics scripts

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global site tag (gtag.js) - Google Analytics */}
+
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=G-BVJS3VNRKR`}
+        strategy="lazyOnLoad"
+      />
+      <Script strategy="lazyOnLoad">
+        {` window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+
+          gtag("config", "G-BVJS3VNRKR");`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
